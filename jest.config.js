fafa72784cd07ff4b357442d222ea26b202f6ee0
fellipySaldanha <fellipy.saldanha@outlook.com>
2023-06-16 07:54:17 -0300

    jest

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePaths: ['/Volumes/ExternalHD/Documents EHD/FIAP/Tech-Challenge/tech-challenge/src'],
+};
