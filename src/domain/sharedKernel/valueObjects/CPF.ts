11c737c24438bb7ef42e6bc6821f40cb734b7672
fellipySaldanha <fellipy.saldanha@outlook.com>
2023-06-27 07:58:44 -0300

    validation adjustment

--- src/domain/sharedKernel/valueObjects/CPF.ts
+++ src/domain/sharedKernel/valueObjects/CPF.ts
@@ -14,25 +14,25 @@ export default class CPF{
         let Soma: number;
         let Resto: number;
         Soma = 0;
-        strCPF = strCPF.replace(/\D/g, '');
+        strCPF = strCPF?.replace(/\D/g, '');
         if (strCPF == "00000000000") return false;
 
         for (let i = 1; i <= 9; i++){
-            Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+            Soma = Soma + parseInt(strCPF?.substring(i - 1, i)) * (11 - i);
         }
         Resto = (Soma * 10) % 11;
 
         if (Resto == 10 || Resto == 11) Resto = 0;
-        if (Resto != parseInt(strCPF.substring(9, 10))) return false;
+        if (Resto != parseInt(strCPF?.substring(9, 10))) return false;
 
         Soma = 0;
         for (let i = 1; i <= 10; i++){
-          Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+          Soma = Soma + parseInt(strCPF?.substring(i - 1, i)) * (12 - i);
         }
         Resto = (Soma * 10) % 11;
 
         if (Resto == 10 || Resto == 11) Resto = 0;
-        if (Resto != parseInt(strCPF.substring(10, 11))) return false;
+        if (Resto != parseInt(strCPF?.substring(10, 11))) return false;
         return true;
     }
 } 
