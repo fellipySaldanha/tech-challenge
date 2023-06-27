10a50a31dc159e355f36f311cef7f32dc4b153d7
fellipySaldanha <fellipy.saldanha@outlook.com>
2023-06-27 09:14:35 -0300

    mysql customer crud

---
+++ src/domain/aggregates/userAccess/infrastructure/MySQLCustomerRepository.ts
@@ -0,0 +1,73 @@
+import mysql from 'mysql';
+import * as dotenv from 'dotenv';
+import ICustomerRepository from '../core/ports/ICustomerRepository';
+
+export default class MySQLCustomerRepository implements ICustomerRepository {
+    private connection:mysql.Connection;
+
+    constructor(){
+        dotenv.config();
+        this.connection = mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_USER,
+            password: process.env.DB_PASSWORD,
+            database: process.env.DB_NAME
+        });
+        this.connection.connect();
+    }
+    
+    async createCustomer(name: string, email: string, cpf: string, active: boolean): Promise<any> {
+        const insertQuery = 'INSERT INTO customers (customer_name, customer_email, customer_cpf, is_active) VALUES (?, ?, ?, ?)';
+        const values = [name, email, cpf, active];
+        const result:any = await this.commitDB(insertQuery,values);
+        return await this.getCustomerById(result?.insertId);
+    }
+
+    async getCustomers(): Promise<any> {
+        const result = await new Promise((resolve, reject) => {
+            this.connection.query('SELECT * FROM customers', (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                resolve(results);
+            });
+        });
+        return result;
+    }
+
+    async getCustomerById(id:number): Promise<any> {
+        const selectQuery = `SELECT * FROM customers WHERE id = ?`;
+        const values = [id];
+        return await this.commitDB(selectQuery,values);
+    }
+
+    async updateCustomer(id:number, name: string, email: string, cpf: string, active: boolean): Promise<any> {
+        const updateQuery = `
+                UPDATE customers
+                SET customer_name = ?, customer_email = ?, customer_cpf = ?, is_active = ?
+                WHERE id = ?
+            `;
+        const values = [name, email, cpf, active,id];
+        return await this.commitDB(updateQuery,values,id);
+    }
+
+    async deleteCustomer(id:number): Promise<any> {
+        const deleteQuery = 'DELETE FROM customers WHERE id = ?';
+        const values = [id];
+        return await this.commitDB(deleteQuery,values);
+    }
+
+    private async commitDB(query:string, values:any[], id?:number){
+        return new Promise((resolve, reject) => {
+            this.connection.query(query, values, (error, results) => {
+                if (error) {
+                    reject(error);
+                }
+                if(id){
+                    results = this.getCustomerById(id);
+                }
+                resolve(results);
+            });
+        });
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import ExpressAdapter from "./application/adapters/ExpressAdapter";
 import CustomerController from "./domain/aggregates/userAccess/application/CustomerController";
-import PostgreCustomerRepository from "./domain/aggregates/userAccess/infrastructure/PostgreCustomerRepository";
+import MySQLCustomerRepository from "./domain/aggregates/userAccess/infrastructure/MySQLCustomerRepository";
 
 const server = new ExpressAdapter();
-const database = new PostgreCustomerRepository();
+const database = new MySQLCustomerRepository();
 const service = new CustomerController(server, database);
 server.router(CustomerController);
 server.listen(3000);
