eb35d339680cec2ab3fc9f21470e562d4d0f8796
eduardopimentaa <eduardopimenta9612@gmail.com>
2023-07-02 22:44:09 -0300

    Inclusão campos descricao e img url e alterações

--- src/domain/aggregates/productMaintenance/core/entities/Product.ts
+++ src/domain/aggregates/productMaintenance/core/entities/Product.ts
@@ -3,11 +3,15 @@ export default class Product {
     itemName: string;
     itemPrice: number;
     itemType: number;
+    itemDescription: string;
+    itemImgUrl:string;
 
-    constructor(itemId: number, itemName: string, itemPrice: number , itemType: number) {
+    constructor(itemId: number, itemName: string, itemPrice: number , itemType: number,itemDescription:string,itemImgUrl:string) {
         this.itemId = itemId;
         this.itemName = itemName;
         this.itemPrice = itemPrice;
         this.itemType = itemType;
+        this.itemDescription = itemDescription;
+        this.itemImgUrl = itemImgUrl;
     }
 }
